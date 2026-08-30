0e5c98cabbaba64804e265003ac07a15e11badbb
cypark7 <pcy6568@gmail.com>
2026-08-30 17:52:18 +0900

    Update app.py

--- app.py
+++ app.py
@@ -15,19 +15,16 @@ st.title("🌞 Prophet Forecast with Preprocessed Sunspot Data")
 # [1] 데이터 불러오기
 # ----------------------------------
 # TODO: 'sunspots_for_prophet.csv' 파일을 불러오고, 'ds' 컬럼을 datetime 형식으로 변환하세요.
-'''코드를 작성하시오'''
 df = pd.read_csv('./sunspots_for_prophet.csv')
 df['ds'] = pd.to_datetime(df['ds'])
 
 st.subheader("📄 데이터 미리보기")
-'''코드를 작성하시오'''
 st.dataframe(df.head())
 
 # ----------------------------------
 # [2] Prophet 모델 정의 및 학습
 # ----------------------------------
 # TODO: Prophet 모델을 생성하고, 11년 주기 커스텀 seasonality를 추가한 후 학습하세요.
-'''코드를 작성하시오'''
 model = Prophet()
 model.add_seasonality(name='sunspot_cycle', period=365.25 * 11, fourier_order=5)
 model.fit(df)
@@ -36,7 +33,6 @@ model.fit(df)
 # [3] 예측 수행
 # ----------------------------------
 # TODO: 30년간 연 단위 예측을 수행하고, 결과를 forecast에 저장하세요.
-'''코드를 작성하시오'''
 future = model.make_future_dataframe(periods=30, freq='Y')
 forecast = model.predict(future)
 
@@ -45,13 +41,11 @@ forecast = model.predict(future)
 # ----------------------------------
 st.subheader("📈 Prophet Forecast Plot")
 # TODO: model.plot()을 사용하여 예측 결과를 시각화하세요.
-'''코드를 작성하시오'''
 fig1 = model.plot(forecast)
 st.pyplot(fig1)
 
 st.subheader("📊 Forecast Components")
 # TODO: model.plot_components()를 사용하여 구성요소를 시각화하세요.
-'''코드를 작성하시오'''
 fig2 = model.plot_components(forecast)
 st.pyplot(fig2)
 
@@ -63,7 +57,6 @@ st.subheader("📉 Custom Plot: Actual vs Predicted with Prediction Intervals")
 # TODO: 실제값, 예측값, 신뢰구간을 하나의 plot에 시각화하세요.
 fig3, ax = plt.subplots(figsize=(14, 6))
 
-'''코드를 작성하시오'''
 # 힌트:
 # ax.plot(df["ds"], df["y"], ...)
 # ax.plot(forecast["ds"], forecast["yhat"], ...)
@@ -82,14 +75,12 @@ st.pyplot(fig3)
 st.subheader("📉 Residual Analysis (예측 오차 분석)")
 
 # TODO: df와 forecast를 'ds' 기준으로 병합하여 residual 컬럼을 생성하세요.
-'''코드를 작성하시오'''
 merged = pd.merge(df, forecast, on='ds', how='inner')
 merged['residual'] = merged['y'] - merged['yhat']
 
 # TODO: residual 시계열을 시각화하세요.
 fig4, ax2 = plt.subplots(figsize=(14, 4))
 
-'''코드를 작성하시오'''
 # 힌트:
 # ax2.plot(merged["ds"], merged["residual"], ...)
 # ax2.axhline(0, ...)
@@ -105,5 +96,4 @@ st.pyplot(fig4)
 # ----------------------------------
 st.subheader("📌 Residual Summary Statistics")
 # TODO: merged["residual"].describe()를 출력하세요.
-'''코드를 작성하시오'''
 st.write(merged['residual'].describe())
